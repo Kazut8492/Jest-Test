a1652fc342cd26278e920d3db95e67ffbe1c6123
Kazuki Taira <kazuki@lift99.co>
2023-01-11 14:03:55 +0200

    component test

--- jest-practice/src/__tests__/Header.test.tsx
+++ jest-practice/src/__tests__/Header.test.tsx
@@ -1,7 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, prettyDOM } from '@testing-library/react'
 import Header from '../components/Header'
  
 test('Propsがない場合はデフォルト文字列が表示される', () => {
-    render(<Header />)
-    screen.debug() ; // eslint-disable-line no-debugger
+    // render(<Header />)
+    render(<Header title={"test"} />)
+    // screen.debug() ; // eslint-disable-line no-debugger
+    console.log(prettyDOM())
 })
